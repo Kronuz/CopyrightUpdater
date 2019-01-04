d283680bed2e91a94441c541992158994706e409
German M. Bravo <german.mb@gmail.com>
2019-01-04 13:38:48 -0600

    Initial commit

---
+++ CopyrightUpdater.py
@@ -0,0 +1,75 @@
+# Copyright (c) 2019 Germán Méndez Bravo (Kronuz). All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+from __future__ import absolute_import
+
+import re
+import datetime
+
+import sublime_plugin
+
+
+COPYRIGHT_RE_PATTERN = r'(Copyright (?:\([cC]\)|©) )([-0-9, ]+)( .+)'
+COPYRIGHT_RE = re.compile(COPYRIGHT_RE_PATTERN)
+
+
+class CopyrightUpdaterCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        view = self.view
+        region = view.find(COPYRIGHT_RE_PATTERN, 0)
+        copyright = view.substr(region)
+        m = COPYRIGHT_RE.match(copyright)
+        if m:
+            prefix, years, suffix = m.groups()
+            dashed = '-' in years
+            comma = ', ' if ', ' in years else ','
+            years_set = set()
+            for year in years.split(','):
+                start, dash, end = year.strip().partition('-')
+                if dash:
+                    year = int(end)
+                    years_set.update(range(int(start), year))
+                else:
+                    year = int(start)
+                years_set.add(year)
+            years_set.add(datetime.datetime.now().year)
+            years_tuples = []
+            for year in sorted(years_set):
+                if dashed and years_tuples and year == years_tuples[-1][-1] + 1:
+                    years_tuples[-1] = (years_tuples[-1][0], year)
+                else:
+                    years_tuples.append((year,))
+            years_strings = []
+            for year in years_tuples:
+                if len(year) == 1:
+                    years_strings.append('{}'.format(year[0]))
+                else:
+                    if year[1] == year[0] + 1:
+                        years_strings.append('{}'.format(year[0]))
+                        years_strings.append('{}'.format(year[1]))
+                    else:
+                        years_strings.append('-'.join('{}'.format(y) for y in year))
+            years = comma.join(years_strings)
+            view.replace(edit, region, prefix + years + suffix)
+
+
+class CopyrightUpdater(sublime_plugin.EventListener):
+    def on_pre_save(self, view):
+        view.run_command('copyright_updater')
---
+++ README.md
@@ -0,0 +1,4 @@
+# Update Copyright
+
+This is a simple plugin which updates the years in the copyright legend when
+the files are saved.
